abd1b426f49ac8026038c303b307269ccf3c9bd6
Migracode <sanchezgalejandroj@gmail.com>
2021-03-16 22:20:59 +0100

    creation of the card component and add content to it with hard code. adding Bootstrap to the project.

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Card.css';
+
+const Card = () => {
+    return (
+        <div className="card">
+            <p >2011</p>
+            <p className="text-center">negro</p>
+            <p className="text-center">#00000</p>
+            <p className="text-end">13-1101</p>
+        </div>
+    )
+}
+
+export default Card
