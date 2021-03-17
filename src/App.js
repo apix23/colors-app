ca389a7dd29346e3c703d8f6ca1a348771604c35
Alejandro Sanchez <sanchezgalejandroj@gmail.com>
2021-03-17 01:11:05 +0100

    creation of the card container and layouting of the card component using flexbox

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import './App.css';
 import Card from './Card';
 import Header from "./Header";
 function App() {
+  const arr = new Array(9)
   return (
     <>
     <Header></Header>
+    <section className="container card__container">
 
+  
     <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    </section>
+
     </>
   );
 }
