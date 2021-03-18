2baee1892d73dad1543a1dfe8e02abe650058215
Alejandro Sanchez <sanchezgalejandroj@gmail.com>
2021-03-18 11:36:53 +0100

    changing the values of the with in the header.css

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./Header.css";
 const Header = () => {
   return (
-    <header className="header mt-3 mb-3 blue-100">
-      <h1>Colors Room</h1>
+    <header className=" user-select-none mt-3 mb-3 blue-100">
+      <h1 className="header">Colors Room</h1>
     </header>
   );
 };
